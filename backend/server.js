33b2350f830ee3c9fc2c61a80d906f36dc3ffd46
RafaelFarand <164330609+RafaelFarand@users.noreply.github.com>
2025-05-17 17:55:45 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ import db from './config/database.js';
 dotenv.config();
 
 const app = express();
+const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 (async () => {
   try {
