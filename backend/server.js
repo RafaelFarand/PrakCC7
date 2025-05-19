ea4eb56bb1a5d12bcbb3ea2fa619961a71d30fa8
RafaelFarand <164330609+RafaelFarand@users.noreply.github.com>
2025-05-19 13:23:02 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'], // atau frontend URL
+  credentials: true
+}));
 app.use(express.json());
 app.use(router);
 app.use(express.static(path.join(__dirname, "../frontend"))); 
