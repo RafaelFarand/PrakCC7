816ba24246c0c463f17d8fffdb06338c4f0bfd98
RafaelFarand <164330609+RafaelFarand@users.noreply.github.com>
2025-05-19 16:00:27 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import router from "./routes/route.js";
 import db from "./config/database.js";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
